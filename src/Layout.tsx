bc512e45238b1d64dbc05c0935ffc66d7e34586b
mansardauna <olaomansur@gmail.com>
2023-10-16 19:11:10 -0400

    hall

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -29,7 +29,7 @@ function Layout() {
       </div>
       <Nav isDark={isDarkMode}/>
 
-      <div className={` md:ml-[20%] md:mt-[5%] mt-[45%] w-full block ${isDarkMode ? 'dark-mode' : ''}`}>
+      <div className={` md:ml-[20%] md:mt-[5%] mt-[40%] w-full block ${isDarkMode ? 'dark-mode' : ''}`}>
         <ScrollRestoration />
         <Outlet />
       </div>
